be1ddcae8bbc3d82082e3260eb737be2f186c9b5
Dermot Sheerin <d.sheerin@hotmail.com>
2022-05-08 14:13:02 +0100

    basic rendering of signup and login with redirects

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Employees = require('../controllers/employees');
+const EmployeesAPI = require('../api/employeesApi');
 const Accounts = require('../controllers/accounts');
 
 /* GET home page. */
@@ -11,6 +12,9 @@ router.get('/', (req, res, next) => {
 
 /* Accounts routes */
 router.get('/login', Accounts.showLogin);
+router.post('/login', Accounts.login);
+router.get('/signup', Accounts.showSignup);
+router.post('/signup', Accounts.signup);
 
 /* GET companies page. */
 router.get('/companies', (req, res, next) => {
@@ -21,5 +25,8 @@ router.get('/companies', (req, res, next) => {
 router.get('/employees', Employees.index);
 router.post('/employees', Employees.addEmployee);
 router.get('/employees/deleteEmployee/:email', Employees.deleteEmployee);
+
+/* API Get all employees */
+router.get('/api/employees', EmployeesAPI.getAllEmployees);
 
 module.exports = router;
